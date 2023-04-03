baaf3aedb32675ddc1eb6787a1f25a494513d89f
danijelaobradovic <danijela.obradovic@htecgroup.com>
2023-04-03 14:27:50 +0200

    Initial commit drugi

---
+++ src/main/java/module-info.java
@@ -0,0 +1,19 @@
+module org.unibl.etf.diamondcircle {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+    requires json.simple;
+    requires dozer;
+    requires java.logging;
+
+    opens com.example.diamondcircle to javafx.fxml;
+    exports com.example.diamondcircle;
+}
